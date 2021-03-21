6760a0113d13ba14fa65911013a4ad270c5f1462
erunesu-nyan <ernest.ascension@gmail.com>
2021-03-21 21:29:19 +0700

    Create migration for comment vote

---
+++ database/migrations/2021_03_21_141206_create_comment_votes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment_votes', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('isUpvote');
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')
+			    ->references('id')
+				->on('users')
+				->onDelete('cascade')
+				->onUpdate('cascade');
+            $table->bigInteger('comment_id')->unsigned();
+            $table->foreign('comment_id')
+                ->references('id')
+                ->on('comments')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment_votes');
+    }
+}
